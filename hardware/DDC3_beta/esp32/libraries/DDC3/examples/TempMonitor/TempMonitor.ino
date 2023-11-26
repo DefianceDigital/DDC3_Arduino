b5ca8b331f02aa713b707bdd297654feff34c326
DefianceDigital <70475060+DefianceDigital@users.noreply.github.com>
2023-11-26 17:10:01 -0500

    added funtions to TempMonitor Example

--- hardware/DDC3_beta/esp32/libraries/DDC3/examples/TempMonitor/TempMonitor.ino
+++ hardware/DDC3_beta/esp32/libraries/DDC3/examples/TempMonitor/TempMonitor.ino
@@ -10,6 +10,8 @@ Adafruit_PCD8544 display = Adafruit_PCD8544(TX, RX, 3, 2, RST);
 
 bool hasDisplay = true; // does device have Nokia5110 Display? Set to 'false' if not
 bool useFahrenheit = true; // if false, celcius will be used
+bool guessAmbTemp = false; // if true, will display estimated ambient temp; if false will display MCU temp
+float AmbTempOffset = 8.8; // [supply voltage dependant] the estimated difference between MCU and Ambient temperature (in celcius)
 
 void initTempSensor(){
     temp_sensor_config_t temp_sensor = TSENS_CONFIG_DEFAULT();
@@ -18,30 +20,34 @@ void initTempSensor(){
     temp_sensor_start();
 }
 
-void setup()   {
-  Serial.begin();
-  
-  initTempSensor();
+void startDisplay(){
+  pinMode(RST, OUTPUT);
+  digitalWrite(RST, LOW);
 
-  if(hasDisplay){
-    pinMode(RST, OUTPUT);
-    digitalWrite(RST, LOW);
+  //Initialize Display
+  display.begin();
 
-    //Initialize Display
-    display.begin();
+  // you can change the contrast around to adapt the display for the best viewing!
+  display.setContrast(59);
 
-    // you can change the contrast around to adapt the display for the best viewing!
-    display.setContrast(59);
+  // Clear the buffer.
+  display.clearDisplay();
 
-    // Clear the buffer.
-    display.clearDisplay();
+  //display.display();
 
-    //display.display();
-
-    display.fillRect(0, 0, 84, 18, BLACK);
-    display.setTextColor(WHITE, BLACK); // 'inverted' text
-    display.setCursor(7,5);
-    display.setTextSize(1);
+  display.fillRect(0, 0, 84, 18, BLACK);
+  display.setTextColor(WHITE, BLACK); // 'inverted' text
+  display.setCursor(7,5);
+  display.setTextSize(1);
+  if(guessAmbTemp){
+    if(useFahrenheit){
+      display.print("Amb Temp (F)");
+      display.display();
+    } else {
+      display.print("Amb Temp (C)");
+      display.display();
+    }
+  } else {
     if(useFahrenheit){
       display.print("MCU TEMP (F)");
       display.display();
@@ -52,10 +58,67 @@ void setup()   {
   }
 }
 
-void loop() {
-  Serial.print("MCU TEMPERATURE: ");
+void switchTemps(){
+  guessAmbTemp ^= 1; // switches to opposite
+  startDisplay();
+
   float result = 0;
   temp_sensor_read_celsius(&result);
+  if(guessAmbTemp){
+    result = result - AmbTempOffset;
+    Serial.print("Amb TEMPERATURE: ");
+  } else {
+    Serial.print("MCU TEMPERATURE: ");
+  }
+  float fahrenheit = (1.8 * result) +32;
+  if(useFahrenheit){
+    Serial.print(fahrenheit);
+    Serial.println(" °F");
+  } else {
+    Serial.print(result);
+    Serial.println(" °C");
+  }
+
+  if(hasDisplay){
+    display.fillRect(0, 19, 84, 29, WHITE);
+    display.setTextSize(2);
+    display.setTextColor(BLACK);
+    display.setCursor(2,25);
+    if(useFahrenheit){
+      display.print(fahrenheit);
+      display.display();
+    } else {
+      display.print(result);
+      display.display();
+    }
+  }
+}
+
+void setup()   {
+  pinMode(BTN, INPUT_PULLUP);
+  
+  Serial.begin();
+  
+  initTempSensor();
+
+  if(hasDisplay){
+    startDisplay();
+  }
+}
+
+void loop() {
+  if(digitalRead(BTN) == LOW){
+    switchTemps(); // if button pressed, switch which one we monitor (hold button until change)
+  }
+  
+  float result = 0;
+  temp_sensor_read_celsius(&result);
+  if(guessAmbTemp){
+    result = result - AmbTempOffset;
+    Serial.print("Amb TEMPERATURE: ");
+  } else {
+    Serial.print("MCU TEMPERATURE: ");
+  }
   float fahrenheit = (1.8 * result) +32;
   if(useFahrenheit){
     Serial.print(fahrenheit);
